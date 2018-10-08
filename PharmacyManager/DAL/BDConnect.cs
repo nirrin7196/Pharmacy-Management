3442d3a395b52eff179e6e6dc2eb2a1c78f8d05c
Jonahh <legiona14@gmail.com>
2018-10-08 22:53:21 +0700

    resolve

--- PharmacyManager/DAL/BDConnect.cs
+++ PharmacyManager/DAL/BDConnect.cs
@@ -1,4 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
-`
-:
+namespace DAL
+{
+    public class BDConnect
+    {
+        public SqlConnection _cn = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=QUANLYNHATHUOC;Integrated Security=True");
+
+    }
+}
